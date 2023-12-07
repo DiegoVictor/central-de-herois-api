d611fb209dc7e36f97d61c0e377e5dc2db2fd732
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-12-07 08:37:31 -0300

    Use lower case

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import 'dotenv/config';
-import Express from 'express';
+import express from 'express';
 import cors from 'cors';
-import Mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 import routes from './routes';
 import { connect } from './websocket';
 
-const App = Express();
+export const app = express();
 
-Mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -16,9 +16,7 @@ Mongoose.connect(process.env.MONGO_URL, {
 
 connect();
 
-App.use(cors());
-App.use(Express.json());
+app.use(cors());
+app.use(express.json());
 
-App.use(routes);
-
-export default App;
+app.use(routes);
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,20 +10,18 @@ import DefeatedController from '../app/controllers/DefeatedController';
 import HeroStore from '../app/validators/Heroes/Store';
 import HeroUpdate from '../app/validators/Heroes/Update';
 
-const Route = Router();
+export const app = Router();
 
-Route.post('/sessions', SessionController.store);
-Route.post('/users', UserController.store);
+app.post('/sessions', SessionController.store);
+app.post('/users', UserController.store);
 
-Route.use(Auth);
+app.use(Auth);
 
-Route.get('/heroes', HeroController.index);
-Route.post('/heroes', HeroStore, HeroController.store);
-Route.put('/heroes/:id', HeroUpdate, HeroController.update);
-Route.delete('/heroes/:id', HeroController.destroy);
+app.get('/heroes', HeroController.index);
+app.post('/heroes', HeroStore, HeroController.store);
+app.put('/heroes/:id', HeroUpdate, HeroController.update);
+app.delete('/heroes/:id', HeroController.destroy);
 
-Route.get('/monsters', MonsterController.index);
+app.get('/monsters', MonsterController.index);
 
-Route.put('/monsters/:id/defeated', DefeatedController.update);
-
-export default Route;
+app.put('/monsters/:id/defeated', DefeatedController.update);
