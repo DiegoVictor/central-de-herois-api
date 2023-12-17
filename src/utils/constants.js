3c27aeda27b51526dd521bb14c74df9bf824e425
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-12-17 11:36:47 -0300

    Create constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,76 @@
+export const INITIAL_RANGE_IN_METERS = 10000;
+
+export const HEROES_NEEDED_BY_MONSTER_LEVEL = {
+  God: [
+    {
+      rank: 'S',
+      quantity: 1,
+    },
+    {
+      rank: 'A',
+      quantity: 2,
+    },
+    {
+      rank: 'B',
+      quantity: 4,
+    },
+    {
+      rank: 'C',
+      quantity: 8,
+    },
+  ],
+  Dragon: [
+    {
+      rank: 'A',
+      quantity: 1,
+    },
+    {
+      rank: 'S',
+      quantity: 1,
+    },
+    {
+      rank: 'B',
+      quantity: 2,
+    },
+    {
+      rank: 'C',
+      quantity: 4,
+    },
+  ],
+  Tiger: [
+    {
+      rank: 'B',
+      quantity: 1,
+    },
+    {
+      rank: 'A',
+      quantity: 1,
+    },
+    {
+      rank: 'S',
+      quantity: 1,
+    },
+    {
+      rank: 'C',
+      quantity: 2,
+    },
+  ],
+  Wolf: [
+    {
+      rank: 'C',
+      quantity: 1,
+    },
+    {
+      rank: 'B',
+      quantity: 1,
+    },
+    {
+      rank: 'A',
+      quantity: 1,
+    },
+    {
+      rank: 'S',
+      quantity: 1,
+    },
+  ],
+};
