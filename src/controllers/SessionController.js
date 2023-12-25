a25391debe65d0aa2523478619008bccc3d5f6bd
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-12-25 11:53:38 -0300

    Use authentication use case

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,39 +1,17 @@
-import jwt from 'jsonwebtoken';
-import bcryptjs from 'bcryptjs';
-
-import { User } from '../models/User';
+import { UserRepository } from '../repositories/user';
+import { AuthenticateUseCase } from '../use-cases/authenticate';
+import { HttpResponse } from '../utils/either/parser';
 
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(404).json({
-        error: {
-          message: 'User not exists',
-        },
-      });
-    }
+    const userRepository = new UserRepository();
+    const authenticateUseCase = new AuthenticateUseCase(userRepository);
 
-    if (!(await bcryptjs.compare(password, user.password))) {
-      return res.status(400).json({
-        error: {
-          message: 'User and/or password not match',
-        },
-      });
-    }
+    const result = await authenticateUseCase.execute({ email, password });
 
-    return res.json({
-      user: {
-        _id: user._id,
-        email: user.email,
-        name: user.name,
-      },
-      token: jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRATION_TIME,
-      }),
-    });
+    return HttpResponse.parse(result, res);
   }
 }
 
