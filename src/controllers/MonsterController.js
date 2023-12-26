c0a674c71ab1e22904dc0cb86b065cf30e4de8c2
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-12-26 09:10:55 -0300

    Use use-case

--- src/controllers/MonsterController.js
+++ src/controllers/MonsterController.js
@@ -1,56 +1,17 @@
-import { Monster } from '../models/Monster';
+import { MonsterRepository } from '../repositories/monster';
+import { GetMonstersUseCase } from '../use-cases/get-monsters';
+import { HttpResponse } from '../utils/either/parser';
 
 class MonsterController {
   async index(req, res) {
     const { status } = req.query;
-    const aggregation = [
-      {
-        $lookup: {
-          from: 'heroes',
-          localField: 'heroes',
-          foreignField: '_id',
-          as: 'heroes',
-        },
-      },
-    ];
 
-    if (status) {
-      aggregation.push({
-        $match: { status },
-      });
-    }
+    const monsterRepository = new MonsterRepository();
+    const getMonstersUseCase = new GetMonstersUseCase(monsterRepository);
 
-    const monsters = await Monster.aggregate(aggregation);
-    return res.json(
-      monsters.map((monster) => {
-        const {
-          _id,
-          name,
-          heroes,
-          rank,
-          location: {
-            coordinates: [longitude, latitude],
-          },
-        } = monster;
-        return {
-          _id,
-          name,
-          heroes: heroes.map((hero) => ({
-            _id: hero._id,
-            name: hero.name,
-            status: hero.status,
-            rank: hero.rank,
-            description: hero.description,
-            longitude: hero.location.coordinates[0],
-            latitude: hero.location.coordinates[1],
-          })),
-          status: monster.status,
-          rank,
-          longitude,
-          latitude,
-        };
-      })
-    );
+    const result = await getMonstersUseCase.execute({ status });
+
+    return HttpResponse.parse(result, res);
   }
 }
 
