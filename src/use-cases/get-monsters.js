113eeba40fe7241a8dc6c069a7deb0e050542d91
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-12-26 09:15:13 -0300

    Add use case to get monsters

---
+++ src/use-cases/get-monsters.js
@@ -0,0 +1,43 @@
+import { success } from '../utils/either/success';
+
+export class GetMonstersUseCase {
+  constructor(monsterRepository) {
+    this.monsterRepository = monsterRepository;
+  }
+
+  async execute({ status }) {
+    const monsters = await this.monsterRepository.aggregateByStatus(status);
+
+    return success(
+      monsters.map((monster) => {
+        const {
+          _id,
+          name,
+          heroes,
+          rank,
+          location: {
+            coordinates: [longitude, latitude],
+          },
+        } = monster;
+
+        return {
+          _id,
+          name,
+          heroes: heroes.map((hero) => ({
+            _id: hero._id,
+            name: hero.name,
+            status: hero.status,
+            rank: hero.rank,
+            description: hero.description,
+            longitude: hero.location.coordinates[0],
+            latitude: hero.location.coordinates[1],
+          })),
+          status: monster.status,
+          rank,
+          longitude,
+          latitude,
+        };
+      })
+    );
+  }
+}
