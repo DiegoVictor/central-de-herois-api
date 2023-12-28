dd5d5b52b009761f7bd291faa5b14580576f31a6
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-12-28 08:41:02 -0300

    Create hero repository

---
+++ src/repositories/hero.js
@@ -0,0 +1,67 @@
+import { model, Schema } from 'mongoose';
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      type: new Schema({
+        type: {
+          type: String,
+          enum: ['Point'],
+          required: true,
+        },
+        coordinates: {
+          type: [Number],
+          required: true,
+        },
+      }),
+      index: '2dsphere',
+    },
+    rank: {
+      type: String,
+      enum: ['S', 'A', 'B', 'C'],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['fighting', 'out_of_combat', 'patrolling', 'resting'],
+      required: true,
+    },
+  },
+  { timestamps: true, collection: 'heroes' }
+);
+
+export const Hero = model('Hero', schema);
+
+export class HeroRepository {
+  async findOneByNameAndUpdate({
+    name,
+    longitude,
+    latitude,
+    rank,
+    status,
+    description,
+  }) {
+    return Hero.findOneAndUpdate(
+      {
+        name,
+      },
+      {
+        name,
+        location: { type: 'Point', coordinates: [longitude, latitude] },
+        rank,
+        status,
+        description,
+      },
+      {
+        upsert: true,
+      }
+    );
+  }
+}
