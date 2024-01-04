c7224823e8df918f7312f651cf46ad699b629f44
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-01-04 11:34:54 -0300

    Create status and rank constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,45 @@
 export const INITIAL_RANGE_IN_METERS = 10000;
+export const MAX_RANGE_IN_METERS = 100000;
+
+export const HERO_STATUS = [
+  'fighting',
+  'out_of_combat',
+  'patrolling',
+  'resting',
+];
+
+export const HERO_STATUS_NAMED = {
+  FIGHTING: 'fighting',
+  OUT_OF_COMBAT: 'out_of_combat',
+  PATROLLING: 'patrolling',
+  RESTING: 'resting',
+};
+
+export const HERO_RANK = ['S', 'A', 'B', 'C'];
+
+export const HERO_RANK_NAMED = {
+  S: 'S',
+  A: 'A',
+  B: 'B',
+  C: 'C',
+};
+
+export const MONSTER_STATUS = ['fighting', 'defeated', 'free'];
+
+export const MONSTER_STATUS_NAMED = {
+  FIGHTING: 'fighting',
+  DEFEATED: 'defeated',
+  FREE: 'free',
+};
+
+export const MONSTER_RANK = ['God', 'Dragon', 'Tiger', 'Wolf'];
+
+export const MONSTER_RANK_NAMED = {
+  GOD: 'God',
+  DRAGON: 'Dragon',
+  TIGER: 'Tiger',
+  WOLF: 'Wolf',
+};
 
 export const HEROES_NEEDED_BY_MONSTER_LEVEL = {
   God: [
