3e88468e9da33466af343e05923b6495008fcd8d
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-01-19 08:44:10 -0300

    Fix lint

--- tests/utils/jwtoken.js
+++ tests/utils/jwtoken.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-export default id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+export default (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRATION_TIME,
   });
-};
