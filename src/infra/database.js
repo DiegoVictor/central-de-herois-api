2ffd5c160c87aa0a777058a92a3c91b1da34e9af
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-04-29 16:17:59 -0300

    Remove deprecated options

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,12 +1,5 @@
 import mongoose from 'mongoose';
 
-const config = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-};
-
 export const setupDatabase = async (cb = null) => {
-  mongoose.connect(process.env.MONGO_URL, config).then(cb);
+  mongoose.connect(process.env.MONGO_URL).then(cb);
 };
